1c0886ea0999a9a253174e155316ba9b84ea2819
WhileBELL <bellbelieve.work@gmail.com>
2024-12-16 08:57:00 +0700

    15/12/24

---
+++ part1/back-end/dbconfig.js
@@ -0,0 +1,19 @@
+var dbconfig = {
+    development: {
+        //connectionLimit : 10,
+        host     : '172.20.10.5',
+        port     : '3306',
+        user     : 'root',
+        password : '',
+        database : 'moviedb'
+    },
+    production: {
+        //connectionLimit : 10,
+        host     : '172.20.10.5',
+        port     : '3306',
+        user     : 'root',
+        password : '',
+        database : 'moviedb'
+    }
+};
+module.exports = dbconfig;
